c531892e47ec52555cdc42ba424769d6ecb7c8c6
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-22 20:23:19 +0800

    fix-搜索和分页功能实现

--- grades/urls.py
+++ grades/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from grades.views import GradeListView
 
 urlpatterns = [
-    path('', GradeListView.as_view(), name='grade_list'),
+    path('', GradeListView.as_view(), name='grades_list'),
 ]
--- grades/views.py
+++ grades/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from grades.models import Grade
 from django.template.loader import get_template
+from django.db.models import Q
 # Create your views here.
 
 class GradeListView(ListView):
@@ -9,3 +10,13 @@ class GradeListView(ListView):
     template_name = 'grades/grades_list.html'
     filed = ['grade_name', 'grade_number']
     context_object_name = 'grades'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(
+                Q(grade_name__icontains=search) | Q(grade_number__icontains=search)
+            )
+        return queryset
