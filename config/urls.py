a6e48b341c3ddb5a355dc1dbb3ee5ab9d2419ee0
LiXiaoYaoCareFree <jp2023213437@qmul.ac.uk>
2025-02-22 23:27:39 +0800

    创建ListView列表页视图类

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('grades/', include('grades.urls')),
+    path('students/', include('students.urls')),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from students.models import Student
+
 
 # Create your views here.
+class StudentListView(ListView):
+    model = Student
+    template_name = 'students/students_list.html'
+    context_object_name = 'students'
+    paginate_by = 10
